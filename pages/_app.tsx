a89795b7bc768ab252a4946537bc78a16085be8b
HynekS <hyneksvacha@seznam.cz>
2022-05-17 18:57:42 +0200

    Add '<MDXEmbedProvider />' wrapper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,13 @@ import type { AppProps } from "next/app"
 import { Global } from "@emotion/react"
 import { GlobalStyles } from "twin.macro"
 import { MDXProvider } from "@mdx-js/react"
+import { MDXEmbedProvider } from "mdx-embed"
 
 import Lightbox from "@/components/Lightbox"
 import { ThemeProvider } from "../components/Theme"
 import baseStyles from "./../components/baseStyles"
+
+import "html5-device-mockups/dist/device-mockups.min.css"
 
 const components = {
   Lightbox,
@@ -18,7 +21,9 @@ function MyApp({ Component, pageProps }: AppProps) {
         <GlobalStyles />
         <Global styles={baseStyles} />
         <MDXProvider components={components}>
-          <Component {...pageProps} />
+          <MDXEmbedProvider>
+            <Component {...pageProps} />
+          </MDXEmbedProvider>
         </MDXProvider>
       </ThemeProvider>
     </>
