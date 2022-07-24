e81eb25e5d3f5e59e2927884fcd2599c8a3f67c1
HynekS <hyneksvacha@seznam.cz>
2022-07-24 08:49:39 +0200

    Reorder twin styles (auto)

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ import { ThemeToggle } from "@/components/Theme"
 const Header = (): JSX.Element => {
   return (
     <header
-      tw="sticky top-0 flex justify-between pr-6 pl-4 lg:(pl-[2.25rem]) pt-6 pb-4 w-full h-[4.5rem]" /* added explicit height and width to fight the layout shift*/
+      tw="top-0 flex justify-between pr-6 pl-4 lg:(pl-[2.25rem] sticky) pt-6 pb-4 w-full h-[4.5rem]" /* added explicit height and width to fight the layout shift*/
     >
       <Link href="/" passHref>
         <a aria-label="Link to homepage">
