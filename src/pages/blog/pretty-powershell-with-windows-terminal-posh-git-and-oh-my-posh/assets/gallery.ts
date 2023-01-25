07bccf54dfd097a1074613db76a4f8eb423aad78
HynekS <hyneksvacha@seznam.cz>
2023-01-25 13:52:19 +0100

    Move gallery images paths from fontmatter to .ts index file from where are they imported

---
+++ src/pages/blog/pretty-powershell-with-windows-terminal-posh-git-and-oh-my-posh/assets/gallery.ts
@@ -0,0 +1,8 @@
+import image1 from "./windows-powershell-pretty-git-prompt-01.png";
+import image2 from "./windows-powershell-pretty-git-prompt-02.png";
+import image3 from "./windows-powershell-pretty-git-prompt-03.png";
+import image4 from "./windows-powershell-pretty-git-prompt-04.png";
+import image5 from "./windows-powershell-pretty-git-prompt-05.png";
+import image6 from "./windows-powershell-pretty-git-prompt-06.png";
+
+export default { image1, image2, image3, image4, image5, image6 };
