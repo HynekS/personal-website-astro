da7ed263b39b607e1a083492ac188307007362df
HynekS <hyneksvacha@seznam.cz>
2023-01-31 08:24:51 +0100

    Clean up (remove comments and dead code)

--- src/pages/blog/components.tsx
+++
@@ -1,7 +0,0 @@
-import BlogPostHeader from "@components/BlogPostHeader.astro";
-import Code from "@components/Code";
-
-export default {
-  pre: (props) => <Code {...props} />,
-  h1: (props) => <BlogPostHeader {...props} />,
-};
