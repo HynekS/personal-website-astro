998defba1d716fd32ef1acf685701e80630bf543
HynekS <hyneksvacha@seznam.cz>
2023-05-05 00:04:40 +0200

    Apply major content/style updates

---
+++ src/content/projects/mulinem-project/assets/gallery.ts
@@ -0,0 +1,7 @@
+import image1 from "./mulinem-front-page.png";
+import image2 from "./mulinem-announcement-page.png";
+import image3 from "./mulinem-sites.png";
+import image4 from "./mulinem-sites-map-expanded.png";
+import image5 from "./mulinem-site.png";
+
+export default { image1, image2, image3, image4, image5 };
