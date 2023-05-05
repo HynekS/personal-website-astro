f423bb990ff5fa1db992b4e31c1c237dee6314c5
HynekS <hyneksvacha@seznam.cz>
2023-05-06 00:13:38 +0200

    Initial commit

---
+++ .vercel/output/static/_astro/Footer.astro_astro_type_script_index_0_lang.70efab02.js
@@ -0,0 +1,1 @@
+const e=document.getElementById("currentYear");e instanceof HTMLElement&&(e.textContent=String(new Date().getFullYear()));
---
+++ .vercel/output/static/_astro/hoisted.d5706d87.js
@@ -0,0 +1,1 @@
+import"./Footer.astro_astro_type_script_index_0_lang.70efab02.js";
