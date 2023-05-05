503e3dc2d4c81efd63868f6ff7a68d914bd3ca7a
HynekS <hyneksvacha@seznam.cz>
2023-05-06 00:14:55 +0200

    Apply minor color tweaks

--- src/components/SunIcon.tsx
+++ src/components/SunIcon.tsx
@@ -2,7 +2,7 @@ const SunIcon = () => (
   <svg
     className="w-6 h-6"
     fill="none"
-    stroke="#9775f9"
+    stroke="#dddddd"
     viewBox="0 0 24 24"
     xmlns="http://www.w3.org/2000/svg"
   >
