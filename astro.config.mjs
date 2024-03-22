11aa824fca427bab22ab16ce79a9d7984f0b6d21
HynekS <hyneksvacha@seznam.cz>
2024-03-22 22:26:29 +0100

    Attempt to fix broken build #3

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
 import preact from "@astrojs/preact";
-import compress from "astro-compress";
+// import compress from "astro-compress";
 import remarkPrism from "remark-prism";
 import defaultLayoutPlugin from "./defaultLayout.mjs";
 import { remarkReadingTime } from "./remark-reading-time.mjs";
@@ -26,6 +26,7 @@ export default defineConfig({
     preact({
       compat: true,
     }),
-    compress(),
+    // breaks build
+    // compress(),
   ],
 });
